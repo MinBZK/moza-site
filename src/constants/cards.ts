b2065739a0307c74cfb06f87e426cfa925a8f7c6
Mick Hoogendam <mick.hoogendam@rijksoverheid.nl>
2025-12-18 09:52:53 +0100

    404 pagina door merge foutje

--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -3,7 +3,8 @@ import type { HomeCard } from "../types/card.ts";
 export const HOME_CARDS: HomeCard[] = [
   {
     title: "MijnOverheid Zakelijk portaal",
-    description: "Maak kennis met de Mijn-omgeving van MOZa via ons digitale prototype.",
+    description:
+      "Maak kennis met de Mijn-omgeving van MOZa via ons digitale prototype.",
     href: "/onderwerpen/portaal",
     imageUrl: "tegel-portaal.svg",
     imageAlt: "",
@@ -11,23 +12,17 @@ export const HOME_CARDS: HomeCard[] = [
   },
   {
     title: "Profiel service",
-    description: "Interactieservice waarmee je op één plek je gegevens en voorkeuren kunt beheren.",
-    href: "/onderwerpen/profielservice",
+    description:
+      "Interactieservice waarmee je op één plek je gegevens en voorkeuren kunt beheren.",
+    href: "/onderwerpen/profiel-service",
     imageUrl: "tegel-profiel.svg",
     imageAlt: "",
     external: false,
   },
-  // {
-  //   title: "Notificatie service",
-  //   description: "Notificatie service voor de gebruiker.",
-  //   href: "#",
-  //   imageUrl: "figma.png",
-  //   imageAlt: "",
-  //   external: false,
-  // },
   {
     title: "Ontwerp",
-    description: "Alles over het MOZa digitale ontwerp, inclusief prototypes voor gebruikersonderzoeken.",
+    description:
+      "Alles over het MOZa digitale ontwerp, inclusief prototypes voor gebruikersonderzoeken.",
     href: "/onderwerpen/ontwerp",
     imageUrl: "tegel-ontwerp.svg",
     imageAlt: "",
@@ -35,15 +30,17 @@ export const HOME_CARDS: HomeCard[] = [
   },
   {
     title: "Open werken",
-    description: "In onze aanpak staat zichtbaar werken en kennisdeling centraal.",
-    href: "/onderwerpen/openwerken",
+    description:
+      "In onze aanpak staat zichtbaar werken en kennisdeling centraal.",
+    href: "/onderwerpen/open-werken",
     imageUrl: "tegel-openwerken.svg",
     imageAlt: "",
     external: false,
   },
   {
     title: "Proeftuin",
-    description: "Onze technische documentatie en achtergrondinformatie handig bij elkaar gezet.",
+    description:
+      "Onze technische documentatie en achtergrondinformatie handig bij elkaar gezet.",
     href: "/onderwerpen/proeftuin",
     imageUrl: "tegel-documentatie.svg",
     imageAlt: "",
